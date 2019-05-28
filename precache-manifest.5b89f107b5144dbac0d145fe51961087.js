6f5acea706004d05132cac2dad191b7b3cb3e0e7
Gustavo Torres <gustavotorresds@gmail.com>
2019-05-28 00:39:22 -0700

    Updates

---
+++ precache-manifest.5b89f107b5144dbac0d145fe51961087.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  },
+  {
+    "revision": "1bedce5882542817fa4c",
+    "url": "/todo/static/js/main.77c0faeb.chunk.js"
+  },
+  {
+    "revision": "bbc7e85cc47ffc08bb47",
+    "url": "/todo/static/js/2.2d0bd611.chunk.js"
+  },
+  {
+    "revision": "1bedce5882542817fa4c",
+    "url": "/todo/static/css/main.4582e3da.chunk.css"
+  },
+  {
+    "revision": "bbc7e85cc47ffc08bb47",
+    "url": "/todo/static/css/2.3ca6c124.chunk.css"
+  },
+  {
+    "revision": "0eb4492258e93e2415b83080abfc8171",
+    "url": "/todo/index.html"
+  }
+];
